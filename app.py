9e16e898681b968a615f0d4510a65b8881903dff
RAFAEL MIRANDA JIMENEZ <91084460+RafaelMJ20@users.noreply.github.com>
2025-06-19 09:25:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 # =======================
 # Config MikroTik
 # =======================
-MIKROTIK_HOST = os.getenv('MIKROTIK_HOST', 'https://49c0-2605-59c8-74d2-e610-00-c8b.ngrok-free.app')
+MIKROTIK_HOST = os.getenv('MIKROTIK_HOST', 'https://e749-2806-370-976e-3572-ac80-781b-8eac-a874.ngrok-free.app')
 USERNAME = os.getenv('MIKROTIK_USER', 'admin')
 PASSWORD = os.getenv('MIKROTIK_PASSWORD', '1234567890')
 REQUEST_TIMEOUT = 10
